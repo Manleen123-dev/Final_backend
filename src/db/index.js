b6bf660adb3c9013f83b0ca2c803a206ea6850c6
Manleen123-dev <kaurmanleen05@gmail.com>
2026-06-15 14:37:50 +0530

    Initial commit

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,38 @@
-import mongoose from "mongoose"
-import { DB_NAME } from "./constants";
-const connectDB=async()=>{
-    try{
-     const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-     console.log(`\n MongoDB connected !! DB HOST:${connectionInstance.connection.host} `)
-    }catch(error){
-        console.log("MONGODB connection error");
-        process.exit(1)
-    }
-}
-export default connectDB
+import dns from "node:dns";
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const DNS_SERVERS = ["8.8.8.8", "1.1.1.1"];
+
+const getMongoUri = () => {
+  if (!process.env.MONGODB_URI) {
+    throw new Error("MONGODB_URI is missing from .env");
+  }
+
+  const mongoUri = new URL(process.env.MONGODB_URI);
+  mongoUri.pathname = `/${DB_NAME}`;
+
+  return mongoUri.toString();
+};
+
+const connectDB = async () => {
+  try {
+    console.log("Trying to connect...");
+
+    dns.setServers(DNS_SERVERS);
+
+    const connectionInstance = await mongoose.connect(getMongoUri(), {
+      family: 4,
+      serverSelectionTimeoutMS: 5000,
+    });
+
+    console.log(
+      `MongoDB Connected! Host: ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,27 +1,19 @@
-//require('dotenv').config({path:'./env'})
-import dotenv from "dotenv"
-import mongoose from "mongoose";
-import { DB_NAME } from "./constants";
-import express from"express"
-import connectDB from "./db"
-const app=express()
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import { app } from "./app.js";
+
+dotenv.config({
+    path: "./.env"
+});
+
+console.log("Starting app...");
 
 connectDB()
-
-/*
-(async()=>{
-   try{
-     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-     app.on("error",(error)=>{
-        console.log("ERRR:",error);
-        throw error
-     })
-     app.listen(process.env.PORT,()=>{
-        console.log(`App is listening on port ${process.env.PORT}`);
-     })
-   } catch(error){
-    console.error("ERROR:",error)
-    throw err
-   }
-})()
-   */
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running at port :${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed!!!",err);
+})
